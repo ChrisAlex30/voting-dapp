ea5c8f77aef3431e9274f6cb2babeb35aedeb496
ChrisAlex30 <akshara155301@gmail.com>
2025-07-19 09:54:40 +0000

    chore: initialize poll with test done

--- anchor/tests/votingdapp.test.ts
+++ anchor/tests/votingdapp.test.ts
@@ -1,27 +1,35 @@
-describe('votingdapp', () => {
-  // TODO: Implement tests for the votingdapp program based on the Codama generated client.
-  // Use tests in `legacy/legacy-next-tailwind-votingdapp/anchor/tests/votingdapp.test.ts` as a reference.
-  it.skip('Initialize Votingdapp', async () => {
-    expect(true).toBe(true)
-  })
+import * as anchor from "@coral-xyz/anchor";
+import {Program} from "@coral-xyz/anchor"
+import {PublicKey} from "@solana/web3.js"
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { Votingdapp} from "../target/types/votingdapp";
+import IDL from "../target/idl/votingdapp.json" assert { type: "json" };
 
-  it.skip('Increment Votingdapp', async () => {
-    expect(true).toBe(true)
-  })
 
-  it.skip('Increment Votingdapp Again', async () => {
-    expect(true).toBe(true)
-  })
 
-  it.skip('Decrement Votingdapp', async () => {
-    expect(true).toBe(true)
-  })
+const votingAdress=new PublicKey("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
+//jest.setTimeout(30000); // 30 seconds
 
-  it.skip('Set votingdapp value', async () => {
-    expect(true).toBe(true)
-  })
+describe("Voting Dapp",()=>{
+  it("Initialize Poll",async()=>{
+    const context=await startAnchor("",[{name:"votingdapp",programId:votingAdress}],[]);
+    const provider=new BankrunProvider(context);
+    const votingProgram=new Program<Votingdapp>(IDL as unknown as Votingdapp,provider);
 
-  it.skip('Set close the votingdapp account', async () => {
-    expect(true).toBe(true)
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "Favourite Peanut Butter: Smooth or Crunchy ?",
+      new anchor.BN(1752914344),
+      new anchor.BN(1762914344),
+    ).rpc();
+
+    const [pollAddress]=PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer,"le",8)],
+      votingAdress
+    );
+
+    const poll=await votingProgram.account.poll.fetch(pollAddress);
+    console.log(poll);
+
   })
 })
