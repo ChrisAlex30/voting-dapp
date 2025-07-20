f93e39038beef8ee12a4f48c95a443e705a554c2
ChrisAlex30 <akshara155301@gmail.com>
2025-07-20 10:50:53 +0000

    chore: candidate init

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -17,7 +17,45 @@ pub mod votingdapp {
         poll.candidate_amount=0;
         Ok(())
     }
+
+    pub fn initialize_candidate(ctx:Context<InitiaizeCandidate>,candidate_name:String,_poll_id:u64)->Result<()>{
+        let candidate=&mut ctx.accounts.candidate;
+        candidate.candidate_name=candidate_name;
+        candidate.candidate_votes=0;
+        let poll=&mut ctx.accounts.poll;
+        poll.candidate_amount+=1;
+        Ok(())
+    }
 }
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate{
+    #[max_len(280)]
+    pub candidate_name:String,
+    pub candidate_votes:u64
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name:String,poll_id:u64)]
+pub struct InitiaizeCandidate<'info>{
+    #[account(mut)]
+    pub signer:Signer<'info>,
+
+    #[account(
+        seeds=[poll_id.to_le_bytes().as_ref()],bump
+    )]
+    pub poll:Account<'info,Poll>,
+
+    #[account(
+        init,payer=signer,space=8+Candidate::INIT_SPACE,
+        seeds=[poll_id.to_le_bytes().as_ref(),candidate_name.as_bytes()],bump
+    )]
+    pub candidate:Account<'info,Candidate>,
+    pub system_program:Program<'info,System>
+}
+
 
 #[account]
 #[derive(InitSpace)]
